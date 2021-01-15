c136d2917513d22e3651e34ad981012bbc73610e
sajjadahmed677 <merledu677@gmail.com>
2021-01-15 22:33:39 +0500

    Make file added for verilator simulation

---
+++ tools/obj_dir/Vazadi_soc_top_tlul_fifo_async.d
@@ -0,0 +1,7 @@
+Vazadi_soc_top_tlul_fifo_async.o: Vazadi_soc_top_tlul_fifo_async.cpp \
+ Vazadi_soc_top_tlul_fifo_async.h \
+ /usr/local/share/verilator/include/verilated_heavy.h \
+ /usr/local/share/verilator/include/verilated.h \
+ /usr/local/share/verilator/include/verilatedos.h Vazadi_soc_top__Syms.h \
+ Vazadi_soc_top.h Vazadi_soc_top_tlul_socket_m1__pi5.h \
+ Vazadi_soc_top_gpio_reg_pkg.h Vazadi_soc_top_tlul_fifo_sync__RCz1_RDz1.h
