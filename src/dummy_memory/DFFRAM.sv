b0727ae81b94352b76a4d6a0703bb96e8f8e7ba4
sajjadahmed677 <merledu677@gmail.com>
2021-01-19 20:54:52 +0500

    DFFRAM issues resolved

--- src/dummy_memory/DFFRAM.sv
+++ src/dummy_memory/DFFRAM.sv
@@ -11,17 +11,17 @@ module DFFRAM (
 
 reg [31:0] mem [0:4096-1];
 
-//initial begin
-//    $readmemh("/home/merl/github_repos/azadi/tools/program.hex", mem);
-//end
-    initial begin
-        mem[0] = 32'h400C0437;
-        mem[1] = 32'h00040413;
-        mem[2] = 32'h00800513;
-        mem[3] = 32'h00A42823;
-       // mem[4] = 32'h00B42E23;
-    //    mem[5] = 32'h00A42E23;
-    end     
+initial begin
+    $readmemh("/home/merl/github_repos/azadi/tools/program.hex", mem);
+end
+  //  initial begin
+  //      mem[0] = 32'h400C0437;
+  //      mem[1] = 32'h00040413;
+  //      mem[2] = 32'h00800513;
+  //      mem[3] = 32'h00A42823;
+  //     // mem[4] = 32'h00B42E23;
+  //  //    mem[5] = 32'h00A42E23;
+  //  end     
 always @(posedge CLK) begin
     if (EN == 1'b1) begin
         Do <= mem[A];
