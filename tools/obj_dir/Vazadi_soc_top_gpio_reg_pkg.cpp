9fe2829bff09cbd971617a8f8ff08cf5eef77e8b
sajjadahmed677 <merledu677@gmail.com>
2021-04-12 13:27:36 +0500

    gitignore file updated

--- tools/obj_dir/Vazadi_soc_top_gpio_reg_pkg.cpp
+++
@@ -1,8 +0,0 @@
-// Verilated -*- C++ -*-
-// DESCRIPTION: Verilator output: Design implementation internals
-// See Vazadi_soc_top.h for the primary calling header
-
-#include "Vazadi_soc_top_gpio_reg_pkg.h"
-#include "Vazadi_soc_top__Syms.h"
-
-//==========
