2765e62f18bb9226dafc6cbd474517823051dece
sajjadahmed677 <merledu677@gmail.com>
2021-04-12 13:47:20 +0500

    PWM and SPI modules added

--- src/dummy_memory/DFFRAM.sv
+++ src/dummy_memory/DFFRAM.sv
@@ -13,7 +13,7 @@ module DFFRAM (
 reg [31:0] mem [0:4096-1];
 
 initial begin
-    $readmemh("/home/merl-lab/fyp/azadi/tests/hex/instruction.hex", mem);
+    $readmemh("../tests/prog.hex", mem);
 end
   //  initial begin
   //      mem[0] = 32'h400C0437;
