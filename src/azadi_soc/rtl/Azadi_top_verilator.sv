ddee72055fd76d141187e8c5f33b536dba827977
zeeshanrafique23 <zeeshanrafique23@gmail.com>
2021-04-14 10:34:33 +0500

    comment clock divider

--- src/azadi_soc/rtl/Azadi_top_verilator.sv
+++ src/azadi_soc/rtl/Azadi_top_verilator.sv
@@ -2,7 +2,7 @@
 module Azadi_top_verilator #(
   parameter  logic DirectDmiTap = 1'b1
 )(
-  input clock_i,
+  input clock,
   input reset_ni,
 
   input  logic [19:0] gpio_i,
@@ -12,21 +12,21 @@ module Azadi_top_verilator #(
   output              uart_tx
 );
 
-logic clock; // output clock after dividing the input clock by divisor
-reg[27:0] counter=28'd0;
-parameter DIVISOR = 28'd5000;
-// The frequency of the output clk_out
-//  = The frequency of the input clk_in divided by DIVISOR
-// For example: Fclk_in = 50Mhz, if you want to get 1Hz signal to blink LEDs
-// You will modify the DIVISOR parameter value to 28'd50.000.000
-// Then the frequency of the output clk_out = 50Mhz/50.000.000 = 1Hz
-always @(posedge clock_i) begin
- counter <= counter + 28'd1;
- if(counter>=(DIVISOR-1))
-  counter <= 28'd0;
+// logic clock; // output clock after dividing the input clock by divisor
+// reg[27:0] counter=28'd0;
+// parameter DIVISOR = 28'd5000;
+// // The frequency of the output clk_out
+// //  = The frequency of the input clk_in divided by DIVISOR
+// // For example: Fclk_in = 50Mhz, if you want to get 1Hz signal to blink LEDs
+// // You will modify the DIVISOR parameter value to 28'd50.000.000
+// // Then the frequency of the output clk_out = 50Mhz/50.000.000 = 1Hz
+// always @(posedge clock_i) begin
+//  counter <= counter + 28'd1;
+//  if(counter>=(DIVISOR-1))
+//   counter <= 28'd0;
 
- clock <= (counter<DIVISOR/2)?1'b1:1'b0;
-end
+//  clock <= (counter<DIVISOR/2)?1'b1:1'b0;
+// end
 
 
 
