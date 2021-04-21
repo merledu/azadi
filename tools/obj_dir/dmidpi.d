f6bd427a3b47eb758766c6aa75e2d265e48c4145
sajjadahmed677 <merledu677@gmail.com>
2021-04-21 14:50:54 +0500

    cpp files removed

--- tools/obj_dir/dmidpi.d
+++
@@ -1,2 +0,0 @@
-dmidpi.o: ../dmidpi.c ../dmidpi.h \
- /usr/share/verilator/include/vltstd/svdpi.h ../tcp_server.h
--- tools/obj_dir/jtagdpi.d
+++
@@ -1,3 +0,0 @@
-jtagdpi.o: ../jtagdpi.c ../jtagdpi.h \
- /usr/share/verilator/include/vltstd/svdpi.h VAzadi_top_verilator__Dpi.h \
- ../tcp_server.h
